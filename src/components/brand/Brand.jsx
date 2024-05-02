d9ec2f856a94bfb7dff1f4307c9b08ddd5189740
Dharaneeswar reddy Rami reddy <DharaneeswarReddy.RamiReddy01@student.csulb.edu>
2024-05-01 18:22:38 -0700

    deploy

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -6,19 +6,19 @@ const Brand = () => {
   return (
     <div className="gpt3__brand section__padding">
       <div>
-        <img src={google} />
+        <img src={google} alt="ggog" />
       </div>
       <div>
-        <img src={slack} />
+        <img src={slack} alt="ggog" />
       </div>
       <div>
-        <img src={atlassian} />
+        <img src={atlassian} alt="ggog" />
       </div>
       <div>
-        <img src={dropbox} />
+        <img src={dropbox} alt="ggog" />
       </div>
       <div>
-        <img src={shopify} />
+        <img src={shopify} alt="ggog" />
       </div>
     </div>
   );
